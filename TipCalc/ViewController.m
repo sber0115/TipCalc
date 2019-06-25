82ccdabf77899668edcb343bb8ce12dff2cd19c3
Sebastian Bernal <sbernal@andrew.cmu.edu>
2019-06-25 16:10:45 -0700

    Testing

--- TipCalc/ViewController.m
+++ TipCalc/ViewController.m
@@ -10,13 +10,81 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *mealBillField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+
 @end
+
+
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+}
+- (IBAction)onTap:(id)sender {
+    NSLog(@"Hello");
+    [self.view endEditing:YES];
+    
+}
+
+- (IBAction)onEdit:(id)sender {
+    
+    NSArray *percentages = @[@(.15),@(.2),@(.22)];
+    
+    double tipPercentage = [percentages[self.tipControl.selectedSegmentIndex] doubleValue];
+    
+    double bill = [self.mealBillField.text doubleValue];
+    double tip = tipPercentage * bill;
+    double total = bill + tip;
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f",tip];
+    
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f",total];
+    
+
+}
+- (IBAction)onEditingBegin:(id)sender {
+    
+    
+    [UIView animateWithDuration:0.2 animations:^{
+    
+        self.mealBillField.frame = CGRectMake(self.mealBillField.frame.origin.x,self.mealBillField.frame.origin.y + 30, self.mealBillField.frame.size.width,self.mealBillField.frame.size.height);
+        
+        self.tipLabel.alpha = 0;
+        
+    }];
+    
+    
+    [UIView animateWithDuration:1  animations:^{
+        self.tipLabel.alpha = 0;
+    }];
+    
+    
+    
+    
+}
+- (IBAction)onEditingEnd:(id)sender {
+    
+    CGRect newFrame = self.mealBillField.frame;
+    
+    newFrame.origin.y -= 30;
+    
+    [UIView animateWithDuration:0.2 animations:^{
+        self.mealBillField.frame = newFrame;
+        
+            
+    }];
+    
+    [UIView animateWithDuration:1  animations:^{
+        self.tipLabel.alpha = 1;
+    }];
+    
+    
 }
 
 
